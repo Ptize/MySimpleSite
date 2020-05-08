d7981f5d0b853ffdaa778e4b549c36f180c72ef0
Ptize <golubevivan-99@mail.ru>
2020-05-08 18:11:00 +0300

    Просто сделал его действительно своим

---
+++ MySimpleSite/Controllers/MyController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySimpleSite.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MyController : Controller
+    {
+        [HttpGet("/myget")]
+        public string MyGet()
+        {
+            var name = $"IVAN";
+            return name;
+        }
+    }
+}
