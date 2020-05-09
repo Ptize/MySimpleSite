83ccf81b8d5682dadf14068e5603c14518b2cb03
Ptize <golubevivan-99@mail.ru>
2020-05-09 12:15:36 +0300

    Можно вводить разные имена

--- myclient/src/components/MyName.js
+++ myclient/src/components/MyName.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import * as actions from "../actions/myName";
-import { withStyles, Typography } from "@material-ui/core";
+import { TextField, withStyles } from "@material-ui/core";
+import useForm from "./useForm";
 
 const styles = (theme) => ({
   name: {
@@ -15,13 +16,47 @@ const styles = (theme) => ({
   },
 });
 
+const initialFieldValues = {
+    fullName: 'IVAN',
+}
+
 const MyName = ({ classes, ...props }) => {
-  useEffect(() => {
-    props.fetchName("IVAN");
+  
+
+  const validate = (fieldValues = values) => {
+    let temp = { ...errors }
+    if ('fullName' in fieldValues)
+        temp.fullName = (/[A-zА-яЁё]/).test(fieldValues.fullName) ? "" : "fullName is not valid."
+    setErrors({
+        ...temp
+    })
+    if (fieldValues == values)
+            return Object.values(temp).every(x => x == "")
+}
+
+const {
+    values,
+    setValues,
+    errors,
+    setErrors,
+    handleInputChange,
+    resetForm
+} = useForm(initialFieldValues, validate, props.setCurrentName)
+
+useEffect(() => {
+    props.fetchName(values.fullName);
   }); //componentDidMount
 
   return (
     <>
+    <TextField
+                        name="fullName"
+                        variant="outlined"
+                        label="Введите имя"
+                        value={values.fullName}
+                        onChange={handleInputChange}
+                        {...(errors.fullName && { error: true, helperText: errors.fullName })}
+                    />
       {props.MyName ? (
         <div className={classes.name}>
             {props.MyName.name}
---
+++ myclient/src/components/useForm.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+
+const useForm = (initialFieldValues, validate, setCurrentName) => {
+    const [values, setValues] = useState(initialFieldValues)
+    const [errors, setErrors] = useState({})
+
+    const handleInputChange = e => {
+        const { name, value } = e.target
+        const fieldValue = { [name]: value }
+        setValues({
+            ...values,
+            ...fieldValue
+        })
+        validate(fieldValue)
+    }
+
+    const resetForm = () => {
+        setValues({
+            ...initialFieldValues
+        })
+        setErrors({})
+        setCurrentName(0)
+    }
+
+    return {
+        values,
+        setValues,
+        errors,
+        setErrors,
+        handleInputChange,
+        resetForm
+    };
+}
+
+export default useForm;
